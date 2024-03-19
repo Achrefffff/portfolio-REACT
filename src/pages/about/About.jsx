bd474e1b7b36638aea904cd419e288b88c76edd6
dorra chaffai <achref.chouikh@gmail.com>
2024-03-19 10:11:44 +0100

    page about terminé

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="container">
+      <h3>" Dieu accorde la victoire à la persévérance ! "</h3> <br />
+      <p>
+        Passionné par l'univers du codage et animé par les évolutions constantes
+        du développement web, j'ai pris la décision audacieuse de changer de cap
+        et de poursuivre ma passion. <br /> <br /> Mon parcours antérieur en
+        physiothérapie m'a inculqué,
+        <strong>
+          
+          des qualités telles que la patience, <br />
+          la résolution de problèmes et le travail d'équipe,
+        </strong>
+        que je m'efforce d'appliquer dans ma nouvelle carrière professionnelle.{" "}
+        <br /> <br /> En plus de ces compétences,
+        <strong>
+          je suis doté d'une grande persévérance et d'une capacité d'adaptation
+          rapide et facile.
+        </strong>
+        <br /> <br /> Je suis convaincu que ces atouts me permettront de relever
+        les défis du monde de la programmation et de m'épanouir dans ce domaine
+        en constante évolution.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
